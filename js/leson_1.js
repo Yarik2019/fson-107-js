5a66f19e357da71857bf68760a8f34c613c76d5d
Yarik2019 <yroslavterno@gmail.com>
2024-05-26 10:55:43 +0300

    add js and html

---
+++ js/leson_1.js
@@ -0,0 +1,31 @@
+//* Розгалуження: if, if...else, else...if
+//TODO: 1 ===================================
+//Даний рядок, що складається із символів, наприклад, 'abcde'.
+// Перевірте, що першим символом цього рядка є буква 'a'.
+// Якщо це так - виведіть 'так', інакше виведіть 'ні'.
+
+const string = "abcde";
+
+// if (string.startsWith("a")) {
+//   console.log("Yes");
+// } else {
+//   console.log("not letter");
+// }
+
+// string.startsWith("a") ? console.log("Yes") : console.log("No");
+
+//TODO: 2 ===================================
+// Якщо ваше ім’я містить більше 4 букв і прізвище більше 5, то виведіть на екран число, яке дорівнює сумі букв імені і прізвища.
+// Якщо умова не виконується - вивести “неможливо виконати умову”.
+// Наприклад: “Віктор Сташко” Віктор - 6 літер, Сташко - 6 літер. Виводимо 12.
+// Використовувати вбудований метод length.
+
+// const firstName = "Vik";
+// const lastName = "Stashko";
+
+// if (firstName.length > 4 && lastName.length > 5) {
+//   const fullLength = firstName.length + lastName.length;
+//   console.log(fullLength);
+// } else {
+//   console.log("неможливо виконати умову");
+// }
