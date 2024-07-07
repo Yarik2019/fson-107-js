601518712cc77b24f6937a64c8030641cf71c709
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2024-07-07 12:14:54 +0300

    add task 2/3-4

--- js/lesson4.js
+++ js/lesson4.js
@@ -1,22 +1,22 @@
 //TODO:=========task-01=================
 // Створіть контейнер div (з класом numberContainer )в HTML-документі та динамічно створіть 100 блоків (з класом number) наповнивши їх рандомними числами від 1 до 100 і додайте їх до контейнера div(numberContainer). Парні числа повинні мати зелений фон (додати клас even), Непарні числа - жовтий фон (додати клас odd).
 
-const divContainer = document.createElement("div");
-divContainer.classList.add("number-container");
+const divContainer = document.createElement('div');
+divContainer.classList.add('number-container');
 const dom = document.body;
 
 const boxs = 200;
 
 const randomNumber = () => Math.floor(Math.random() * 100) + 1;
 for (let i = 0; i < boxs; i++) {
-  const box = document.createElement("div");
+  const box = document.createElement('div');
   const randoms = randomNumber();
-  box.classList.add("number");
+  box.classList.add('number');
   box.textContent = randoms;
   if (randoms % 2 === 0) {
-    box.classList.add("even");
+    box.classList.add('even');
   } else {
-    box.classList.add("odd");
+    box.classList.add('odd');
   }
   divContainer.appendChild(box);
 }
@@ -38,17 +38,17 @@ dom.prepend(divContainer);
 ** 4 - При події `submit`. Відміни поведінку браузера по змовчуванню. Дістань данні з інпуту і чек боксу, зроби перевірку, що інпут не порожній, також, що нажатий чек бокс у положення true, якщо користувач все виконав вірно, збери данні (userName) у обьект і виведи у консоль. У разі, якщо користувач не виконав одну із умов, виведи повідомлення. Також реалізуй додавання ім`я користувача у span, замість слова "Anonymous". Якщо користувач ввів ім`я, а потім видали, зроби так, щоб на місце повернулось дефолтне знаяення "Anonymous". При відправці форми, очисти інпут, верни чек бокс у положення false, верни дефолтне значення "Anonymous" у span.
  */
 
-const formEl = document.querySelector(".js-contact-form");
+// const formEl = document.querySelector(".js-contact-form");
 
 // formEl.addEventListener("input", onFormInput);
 
-// function onFormInput(event){ 
+// function onFormInput(event){
 //   const inputValue = event.target.value.trim();
 //   if (inputValue.length > 6) {
 //     event.target.classList.add("success");
 //     event.target.classList.remove("error");
 
-//   } else { 
+//   } else {
 //     event.target.classList.remove("success");
 //     event.target.classList.add("error");
 //   }
@@ -62,12 +62,54 @@ const formEl = document.querySelector(".js-contact-form");
 
 //   !target.value.trim() ? target.style.outline = '3px solid red' : target.style.outline = '3px solid green';
 
-  // if (!target.value.trim()) {
-  //   target.style.outline = '3px solid red';
-  // } else { 
-  //   target.style.outline = '3px solid green';
-  // }
+// if (!target.value.trim()) {
+//   target.style.outline = '3px solid red';
+// } else {
+//   target.style.outline = '3px solid green';
+// }
+// }
+
+const inputFild = document.querySelector('.js-username-input');
+inputFild.addEventListener('blur', onBlur);
+
+function onBlur(event) {
+  const target = event.target;
+
+  !target.value.trim()
+    ? (target.style.outline = '3px solid red')
+    : (target.style.outline = '3px solid green');
 }
+
+const formEl = document.querySelector('.js-contact-form');
+const spanEl = document.querySelector('.js-username-output');
+
+formEl.addEventListener('submit', onSubmit);
+formEl.addEventListener('input', onInput);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const checkBox = event.currentTarget.elements.accept.checked;
+  const userInput = event.currentTarget.elements.userName;
+
+  if (!userInput.value.trim() || !checkBox) {
+    return alert('Введіть дані');
+  }
+
+  console.log({ userName: userInput.value.trim() });
+  event.currentTarget.reset();
+  spanEl.textContent = 'Anonimus';
+  userInput.style.border = '1px solid #cccccc';
+  userInput.style.outline = 'none';
+}
+
+function onInput(event) {
+  const inputValue = event.currentTarget.elements.userName.value.trim();
+  spanEl.textContent = inputValue;
+  if (!inputValue) {
+    spanEl.textContent = 'Anonimus';
+  }
+}
+
 //TODO:=========task-03=================
 // Кнопка "Зменшити" робить квадрат меншим на 20 пікселів, кнопка "Збільшити" - робить більшим на 10 пікселів.
 
@@ -76,29 +118,29 @@ const formEl = document.querySelector(".js-contact-form");
 
 const films = [
   {
-    title: "Tetris",
-    imgUrl: "https://static.hdrezka.ac/i/2023/3/20/f509264b419fdmu53x38j.jpg",
-    id: "film_1",
+    title: 'Tetris',
+    imgUrl: 'https://static.hdrezka.ac/i/2023/3/20/f509264b419fdmu53x38j.jpg',
+    id: 'film_1',
   },
   {
-    title: "Avatar: The Way of Water",
-    imgUrl: "https://static.hdrezka.ac/i/2022/12/22/tc5e6b8212683gn66r84s.jpg",
-    id: "film_2",
+    title: 'Avatar: The Way of Water',
+    imgUrl: 'https://static.hdrezka.ac/i/2022/12/22/tc5e6b8212683gn66r84s.jpg',
+    id: 'film_2',
   },
   {
-    title: "Operation Fortune: Ruse de guerre",
-    imgUrl: "https://static.hdrezka.ac/i/2022/2/11/s0d53f6cf0ae0tq29m85l.jpg",
-    id: "film_3",
+    title: 'Operation Fortune: Ruse de guerre',
+    imgUrl: 'https://static.hdrezka.ac/i/2022/2/11/s0d53f6cf0ae0tq29m85l.jpg',
+    id: 'film_3',
   },
   {
-    title: "Babylon",
-    imgUrl: "https://static.hdrezka.ac/i/2022/12/25/z330b47a82209ww99w55a.jpg",
-    id: "film_4",
+    title: 'Babylon',
+    imgUrl: 'https://static.hdrezka.ac/i/2022/12/25/z330b47a82209ww99w55a.jpg',
+    id: 'film_4',
   },
   {
-    title: "The Whale",
-    imgUrl: "https://static.hdrezka.ac/i/2023/2/24/h23d8c65d734akd89q94c.jpg",
-    id: "film_5",
+    title: 'The Whale',
+    imgUrl: 'https://static.hdrezka.ac/i/2023/2/24/h23d8c65d734akd89q94c.jpg',
+    id: 'film_5',
   },
 ];
 
@@ -110,7 +152,7 @@ const films = [
 </li> */
 }
 
-const watchedFilms = ["film_2", "film_4", "film_5"];
+const watchedFilms = ['film_2', 'film_4', 'film_5'];
 
 //TODO:=========task-05======Modal window===========
 /**
